3a429b6ae71498c1bb3430a06a14cddbb2cd34d6
Moonlight <moonlight9512@gmail.com>
2026-03-14 16:43:17 +0500

    Add end-point to view synonyms list

--- src/main/java/com/github/tavi/lilbird/api/controllers/BirdIndexApi.java
+++ src/main/java/com/github/tavi/lilbird/api/controllers/BirdIndexApi.java
@@ -5,14 +5,22 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.github.tavi.lilbird.db.entities.BirdEntry;
+import com.github.tavi.lilbird.db.entities.BirdSynonym;
 import com.github.tavi.lilbird.services.BirdIndexService;
 
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Min;
 
 
 /**
@@ -21,6 +29,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponse;
  */
 @RestController
 @RequestMapping("api/birds")
+@Validated
 public class BirdIndexApi {
 
     @Autowired
@@ -38,6 +47,48 @@ public class BirdIndexApi {
     )
     public ResponseEntity<List<BirdEntry>> listAll() {
         final List<BirdEntry> entryList = service.getEntryList();
+        return ResponseEntity.ok(entryList);
+    }
+
+    @ApiResponses(value = {
+        @ApiResponse(
+            description = "Returns a JSON array of alternative names for the bird",
+            responseCode = "200",
+            useReturnTypeSchema = true
+        ),
+        @ApiResponse(
+            description = "Empty array, if a bird by this id does not exist",
+            responseCode = "400",
+            content = @Content(
+                schema = @Schema(example = "[]")
+            )
+        )
+    })
+    @GetMapping(
+        value = "/names-of/{id}", 
+        produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public ResponseEntity<List<BirdSynonym>> synonymsOf(
+            @Valid
+            @Min(
+                value = 1,
+                message = "The ID is always > 0"
+            )
+            @Schema(
+                description = "The ID of an existing bird entry",
+                example = "1"
+            )
+            @PathVariable("id") 
+            final long id
+        ) 
+    {
+        final List<BirdSynonym> entryList = service.getSynonymsOf(id);
+        if (entryList.isEmpty()) {
+            return ResponseEntity
+                    .badRequest()
+                    .body(entryList);
+        }
+
         return ResponseEntity.ok(entryList);
     }
 
